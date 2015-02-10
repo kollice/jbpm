19d66b7e4e8ac6254815d0114ed08a07d1e56559
baijianye <baijianye@baijianye.com>
2015-02-10 08:57:15 +0800

    初始化

---
+++ JbpmTest/src/com/kollice/jbpm/chapter5/ProcessEngineTest.java
@@ -0,0 +1,25 @@
+package com.kollice.jbpm.chapter5;
+
+import org.jbpm.test.JbpmTestCase;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ProcessEngineTest extends JbpmTestCase {
+
+	@Before
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	@After
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	@Test
+	public void test() {
+		
+	}
+
+}
